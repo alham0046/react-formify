4da28c8c197f96fd1fe19b135171367a8b716561
alham0046 <alham.kota@gmail.com>
2025-10-08 13:24:30 +0530

    useFormInitial support for dot notation 1

--- src/hooks/useFormInitialState.ts
+++ src/hooks/useFormInitialState.ts
@@ -4,8 +4,7 @@ interface InitialState {
     [key: string]: any;
 }
 
-function expandDotNotation(obj: Record<string, any>) {
-    const result: Record<string, any> = {};
+function expandDotNotation(obj: Record<string, any>, initialData: Record<string, any>) {
 
     for (const key in obj) {
         const value = obj[key];
@@ -18,12 +17,11 @@ function expandDotNotation(obj: Record<string, any>) {
                 acc[currKey] = acc[currKey] || {};
             }
             return acc[currKey];
-        }, result);
+        }, initialData);
     }
 
-    return result;
+    return initialData;
 }
-
 
 export const useFormInitials = (initialState: InitialState): void => {
     const keys = Object.keys(initialState);
@@ -36,13 +34,68 @@ export const useFormInitials = (initialState: InitialState): void => {
             }
         }))
     }
-    else {
-        const expanded = expandDotNotation(initialState);
+    else {        
         useInputStore.setState((state) => ({
             inputData: {
                 ...state.inputData,
-                ...expanded
+                ...expandDotNotation(initialState, state.inputData)
             }
         }))
     }
 }
+
+
+
+// import { useInputStore } from "./useInputStore"
+
+// interface InitialState {
+//     [key: string]: any;
+// }
+
+// function expandDotNotation(obj: Record<string, any>) {
+//     const result: Record<string, any> = {};
+
+//     for (const key in obj) {
+//         const value = obj[key];
+//         const keys = key.split('.');
+
+//         keys.reduce((acc, currKey, index) => {
+//             if (index === keys.length - 1) {
+//                 acc[currKey] = value;
+//             } else {
+//                 acc[currKey] = acc[currKey] || {};
+//             }
+//             return acc[currKey];
+//         }, result);
+//     }
+
+//     return result;
+// }
+
+// const expDotExpand = () => {
+
+// }
+
+
+// export const useFormInitials = (initialState: InitialState): void => {
+//     const keys = Object.keys(initialState);
+//     const hasDot = keys.some(key => key.includes('.'));
+//     if (!hasDot) {
+//         useInputStore.setState((state) => ({
+//             inputData: {
+//                 ...state.inputData,
+//                 ...initialState
+//             }
+//         }))
+//     }
+//     else {
+        
+//         const expanded = expandDotNotation(initialState);
+//         useInputStore.setState((state) => ({
+//             inputData: {
+//                 ...state.inputData,
+//                 ...expanded
+//             }
+//         }))
+//     }
+// }
